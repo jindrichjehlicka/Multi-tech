9459f332c6d8befb782102626feb3fd2e6faaf85
Jindrich Jehlicka <jindra.jehlicka@gmail.com>
2017-09-24 03:04:38 +0200

    Register page and disabling eslint in couple pages

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -18,12 +18,18 @@ module.exports = {
   'settings': {
     'import/resolver': {
       'webpack': {
-        'config': 'build/webpack.base.conf.js'
+        // 'config': 'build/webpack.base.conf.js'
       }
     }
   },
   // add your custom rules here
   'rules': {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "no-mixed-spaces-and-tabs": [0],
+    "no-tabs": 0,
+    "skipBlankLines": 0,
+    "ignoreComments": 0,
+    "no-trailing-spaces": [2, { "skipBlankLines": true }],
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       'js': 'never',
--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
-const express = require ('express')
-const bodyParser = require ('body-parser')
+const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
-const morgan = require ('morgan')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -9,10 +9,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status',(req,res) =>{
-res.send({
-    message:'Hello world'
-})
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email}, your user was registered`
+    })
 })
 
 app.listen(process.env.PORT || 8081)
