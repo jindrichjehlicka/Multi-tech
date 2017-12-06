c0f227a42340b17ed1e6fe10a00e875426df0613
Jindrich Jehlicka <jindra.jehlicka@gmail.com>
2017-12-06 23:16:48 +0100

    display owned products - working

--- client/src/components/Products/ProductsManuals.vue
+++ client/src/components/Products/ProductsManuals.vue
@@ -1,7 +1,36 @@
 <template>
 
     <panel title="Manuals">
-    <v-data-table
+    <div 
+      v-for="manual in manuals"
+      class="manual"
+      :key="manual.id"
+      >
+
+      <v-layout>
+        <v-flex xs12 sm6>
+            <div class="product-name">
+              {{manual.companyName}}
+              </div>
+              <div class="product-name">
+              {{manual.model}}
+              </div>
+</v-flex>
+ <v-flex sm6 hidden-xs-only>
+            <img class="product-logo mt-4" :src="manual.companyLogo"/>
+            
+          </v-flex>
+             
+               
+
+              
+
+          
+          
+        </v-layout>
+<v-divider class="grey mt-5"></v-divider>
+      </div>
+    <!--<v-data-table
     :headers="headers"
     :pagination.sync ="pagination"
     :items="manuals">
@@ -15,7 +44,7 @@
     {{props.item.model}}
     </td>
     </template>
-    </v-data-table>
+    </v-data-table>-->
     </panel>
 </template>
 
@@ -25,21 +54,22 @@ import ManualsService from '@/services/ManualsService'
     export  default {
         data () {
             return {
-                headers: [
-                    {
-                        text:'Name',
-                        value: 'companyName'
-                    },
-                    {
-                        text:'Model',
-                        value:'model'
-                    }
-                ],
-                pagination: {
-                    sortBy: 'date',
-                    descending: true
-                },
-                manuals: []
+                // headers: [
+                //     {
+                //         text:'Name',
+                //         value: 'companyName'
+                //     },
+                //     {
+                //         text:'Model',
+                //         value:'model'
+                //     }
+                // ],
+                // pagination: {
+                //     sortBy: 'date',
+                //     descending: true
+                // },
+                manuals: [],
+                products:null
             }
         },
         computed: {
@@ -53,11 +83,35 @@ import ManualsService from '@/services/ManualsService'
                     userId: this.$store.state.user.id
                 })).data
             }
-        }
+        },
+//          watch: {
+//     '$route.query.search':  {
+//       immediate: true,
+//       async handler (value) {
+//         this.manuals = (await ManualsService.index(value)).data
+//       }
+//     }
+//   },
         
     }
 </script>
 
 <style scoped>
-    
+.product{
+  padding:30px;
+  height: 230px;
+  overflow: hidden;
+}
+
+.product-name{
+  font-size:30px;
+}
+
+.product-model{
+font-size:24px;
+}
+    .product-logo{
+  width:100%;
+  margin: 0 auto;
+}
 </style>
